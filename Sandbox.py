23989cb073c06aec37001ce84c5f928900338f61
Jack <ingalls@stanford.edu>
2013-09-06 16:38:06 -0700

    Added sandbox
    Added a sandbox to test out github functionality.

---
+++ Sandbox.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This temporary script file is located here:
+C:\Users\Jack\.spyder2\.temp.py
+"""
+
+greeting = "Hello World!"
+
+print greeting
+for i in range(7):
+    print i
+    
+for c in greeting:
+    print c
+
+import math
